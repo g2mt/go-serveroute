5692356013e3e6661d3eacc7c9b1b9edd125f8dc
g2mt <pm2mtr@gmail.com>
2026-02-26 13:44:58 +0000

    refactor: events

--- event.go
+++ event.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
 	"sync"
-
-	"github.com/gin-contrib/sse"
 )
 
 type Event struct {
@@ -54,50 +50,5 @@ func (eb *EventBus) Unsubscribe(id int64) {
 	if ch, ok := eb.events[id]; ok {
 		close(ch)
 		delete(eb.events, id)
-	}
-}
-
-func (s *Server) apiEvents(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", sse.ContentType)
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	id, ch := s.EventBus.Subscribe()
-	defer s.EventBus.Unsubscribe(id)
-
-	sse.Encode(w, sse.Event{
-		Event: "connected",
-		Data:  "connected",
-	})
-
-	if flusher, ok := w.(http.Flusher); ok {
-		flusher.Flush()
-	}
-
-	// Listen for events and send them to client
-	for {
-		select {
-		case event := <-ch:
-			data, err := json.Marshal(event)
-			if err != nil {
-				return
-			}
-
-			if err := sse.Encode(w, sse.Event{
-				Event: "message",
-				Data:  string(data),
-			}); err != nil {
-				return
-			}
-
-			// Flush to ensure the event is sent immediately
-			if flusher, ok := w.(http.Flusher); ok {
-				flusher.Flush()
-			}
-		case <-r.Context().Done():
-			// Client disconnected
-			return
-		}
 	}
 }
--- server.go
+++ server.go
@@ -11,10 +11,12 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/gin-contrib/sse"
 )
 
 type Server struct {
-	Config   *Config
+	Config   *Config // readonly
 	Services map[string]*ServiceState
 	Mu       sync.RWMutex
 	EventBus *EventBus
@@ -220,26 +222,70 @@ func (s *Server) apiListServices(w http.ResponseWriter) {
 
 	result := make(map[string]interface{})
 
-	for name, state := range s.Services {
-		if state.Service.Hidden {
+	for name, service := range s.Config.Services {
+		if service.Hidden {
 			continue
 		}
 
-		running := state.IsRunning()
+		state := s.Services[name]
 		state.Mu.Lock()
+		defer state.Mu.Unlock()
+
 		status := "stopped"
-		if running {
+		if state.IsRunning() {
 			status = "started"
 		}
 
-		subdomain := state.Service.Subdomain
-		state.Mu.Unlock()
-
 		result[name] = map[string]interface{}{
 			"status":    status,
-			"subdomain": subdomain,
+			"subdomain": service.Subdomain,
 		}
 	}
 
 	json.NewEncoder(w).Encode(result)
+}
+
+func (s *Server) apiEvents(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", sse.ContentType)
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	id, ch := s.EventBus.Subscribe()
+	defer s.EventBus.Unsubscribe(id)
+
+	sse.Encode(w, sse.Event{
+		Event: "connected",
+		Data:  "connected",
+	})
+
+	if flusher, ok := w.(http.Flusher); ok {
+		flusher.Flush()
+	}
+
+	// Listen for events and send them to client
+	for {
+		select {
+		case event := <-ch:
+			data, err := json.Marshal(event)
+			if err != nil {
+				return
+			}
+
+			if err := sse.Encode(w, sse.Event{
+				Event: "message",
+				Data:  string(data),
+			}); err != nil {
+				return
+			}
+
+			// Flush to ensure the event is sent immediately
+			if flusher, ok := w.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		case <-r.Context().Done():
+			// Client disconnected
+			return
+		}
+	}
 }
